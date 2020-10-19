2856f52614e90ffadadba0e41539613ecf5c9ec2
Vedang21 <64269237+Vedang21@users.noreply.github.com>
2020-10-19 15:47:27 +0530

    Add files via upload

---
+++ C97 Introduction To Python/countingWords.py
@@ -0,0 +1,14 @@
+install=input("enter your introduction")
+print(install)
+
+cc=0
+wc=1
+
+for me in install:
+    cc =cc+1
+    if(me==' '):
+        wc =wc+1
+print("number of words")
+print(wc)       
+print("number of characters") 
+print(cc)
